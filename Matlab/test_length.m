8a7fb86081d813b615dc1d194bac399e20db2de6
Joterom <j.oterom@alumnos.upm.es>
2018-09-25 18:33:13 +0200

    2.1.1

--- Matlab/test_length.m
+++ Matlab/test_length.m
@@ -1,4 +1,4 @@
-function [error_f, error_t] = test_length(ventana)
+function test_length(pot)
 %TEST_LENGTH Prueba diferentes tamaños de ventana en la FFT
 %   Permite realizar la FFT del archivo de entrada especificada con un
 %   tamaño de ventana especificado como parametro.
@@ -6,7 +6,8 @@ function [error_f, error_t] = test_length(ventana)
 close all;
 
 FILE_IN = 'audio_prueba.wav';
-FILE_OUT = 'tested_window_audio.wav';
+FILE_OUT_FULL = 'full_window_audio.wav';
+FILE_OUT_TEST = 'tested_window_audio.wav';
 
 [y, Fs] = audioread(FILE_IN);
 L = y(:,1);
@@ -15,103 +16,49 @@ R = y(:,2);
 Ts = 1/Fs;
 max_time = (length(L)*Ts)-Ts;
 t = 0:Ts:max_time;
-f = Fs*(0:(length(L)/2))/length(L);
+ventana = 2^pot;
 
-f1 = figure('Name','Stereo input signal','NumberTitle','off');
-f3 = figure('Name','Full Vs Specified length FFT','NumberTitle','off');
 f4 = figure('Name','Stereo output signal','NumberTitle','off');
 
-% Graficas de la entrada
-figure(f1);
-hold on;
-subplot(3,1,1), plot(t,y), title('Both Channels'), xlabel('Time(s)'), ylabel('Amplitude');
-subplot(3,1,2), plot(t,L), title('Left Channel'), xlabel('Time(s)'), ylabel('Amplitude');
-subplot(3,1,3), plot(t,R), title('Right Channel'),xlabel('Time(s)'), ylabel('Amplitude');
-hold off;
-
 %Transformada directa para comprobar
-comprobacion_freq = abs(fft(y));
-comprobacion_t = real(ifft(comprobacion_freq));
+comprobacion = octave(y,Fs);
 
-PLaux = abs(comprobacion_freq(:,1)/length(L));
-PL = PLaux(1:length(L)/2+1);
-PL(2:end-1) = 2*PL(2:end-1);
-PL_max = max(PL);
-PL_plot_full = PL./PL_max;
-
-PRaux = abs(comprobacion_freq(:,2)/length(R));
-PR = PRaux(1:length(R)/2+1);
-PR(2:end-1) = 2*PR(2:end-1);
-PR_max = max(PR);
-PR_plot_full = PR./PR_max;
 
 %Transformada con la ventana especificada
 num_bloques = floor(length(y)/ventana);
 ultimo_bloque = mod(length(y),ventana);
-result_L = zeros(size(L)); 
-result_R = zeros(size(R)); 
+result = zeros(size(y)); 
 
 for i = 0:num_bloques-1
-    operandoL = L(i*ventana + 1:(i + 1)*ventana, 1);
-    operandoR = R(i*ventana + 1:(i + 1)*ventana, 1);
-    transformed_L = fft(operandoL);
-    transformed_R = fft(operandoR);
-    result_L(i*ventana + 1:(i + 1)*ventana, 1) = real(ifft(transformed_L));
-    result_R(i*ventana + 1:(i + 1)*ventana, 1) = real(ifft(transformed_R));
+    operando = y(i*ventana + 1:(i + 1)*ventana, :);
+    result(i*ventana + 1:(i + 1)*ventana, :) = octave(operando,Fs);
 end
+
 % Ultimas muestras y padding
-operandoL = zeros(ventana,1); 
-operandoR = zeros(ventana,1);
-operandoL(1:ultimo_bloque,1) = L(ventana*num_bloques + 1:end);
-operandoR(1:ultimo_bloque,1) = R(ventana*num_bloques + 1:end);
-transformed_L = fft(operandoL);
-transformed_R = fft(operandoR);
-time_L = real(ifft(transformed_L));
-time_R = real(ifft(transformed_R));
-result_L(num_bloques*ventana + 1:end, 1) = time_L(1:ultimo_bloque,1);
-result_R(num_bloques*ventana + 1:end, 1) = time_R(1:ultimo_bloque,1);
+operando = zeros(ventana,2); 
+operando(1:ultimo_bloque,:) = y(ventana*num_bloques + 1:end, :);
+oct = octave(operando,Fs);
+result(num_bloques*ventana + 1:end, :) = oct(1:ultimo_bloque,:);
 
-%Graficamos para comparar
-PLaux = abs(comprobacion_freq(:,1)/length(L));
-PL = PLaux(1:length(L)/2+1);
-PL(2:end-1) = 2*PL(2:end-1);
-PL_max = max(PL);
-PL_plot = PL./PL_max;
-
-PRaux = abs(comprobacion_freq(:,2)/length(R));
-PR = PRaux(1:length(R)/2+1);
-PR(2:end-1) = 2*PR(2:end-1);
-PR_max = max(PR);
-PR_plot = PR./PR_max;
-
-figure(f3);
-hold on;
-subplot(4,1,1), plot(f, PL_plot_full), title('Full length FFT Left'), xlabel('Frequency(Hz)');
-subplot(4,1,2), plot(f, PR_plot_full), title('Full length FFT Right'),xlabel('Frequency(Hz)');
-subplot(4,1,3), plot(f, PL_plot), title([num2str(ventana) ,' points FFT Left']), xlabel('Frequency(Hz)');
-subplot(4,1,4), plot(f, PR_plot), title([num2str(ventana) ,' points FFT Right']), xlabel('Frequency(Hz)');
-hold off;
-
-% Montaje de la señal final
-out(:,1) = result_L(:,1);
-out(:,2) = result_R(:,1);
-
-error_t = mean(abs(out./comprobacion_t));
-error_f = (mean(abs(result_L./comprobacion_freq(:,1))) + mean(abs(result_L./comprobacion_freq(:,1))))/2;
 
 %Graficamos la salida
 figure(f4);
 hold on;
-subplot(3,1,1), plot(t,out), title('Both Channels'), xlabel('Time(s)'), ylabel('Amplitude');
-subplot(3,1,2), plot(t,result_L), title('Left Channel'), xlabel('Time(s)'), ylabel('Amplitude');
-subplot(3,1,3), plot(t,result_R), title('Right Channel'),xlabel('Time(s)'), ylabel('Amplitude');
+subplot(3,2,1), plot(t,comprobacion), title('Full length FFT BOTH'), ylabel('Amplitude');
+subplot(3,2,3), plot(t,comprobacion(:,1)), title('Full length FFT LEFT'), ylabel('Amplitude');
+subplot(3,2,5), plot(t,comprobacion(:,2)), title('Full length FFT RIGHT'), ylabel('Amplitude');
+
+subplot(3,2,2), plot(t,result), title('Specified length BOTH'), ylabel('Amplitude');
+subplot(3,2,4), plot(t,result(:,1)), title('Specified length LEFT'), ylabel('Amplitude');
+subplot(3,2,6), plot(t,result(:,2)), title('Specified length RIGHT'), xlabel('Time(s)'), ylabel('Amplitude');
 hold off;
 
 % Reproducimos
 % sound(out,Fs);
 
 % Guardamos en un archivo .wav
-audiowrite(FILE_OUT, out, Fs);
+audiowrite(FILE_OUT_FULL, comprobacion, Fs);
+audiowrite(FILE_OUT_TEST, result, Fs);
 
 
 
