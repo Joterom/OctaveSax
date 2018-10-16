ec1f65ed0ba466d96ee0b90dabf3d865d41730c8
Joterom <oterosaxo@gmail.com>
2018-10-16 16:58:30 +0200

    2.2.7.2

--- Matlab/prueba_algoritmo.m
+++ Matlab/prueba_algoritmo.m
@@ -2,9 +2,13 @@ function prueba_algoritmo
 %PRUEBA_ALGORITMO Summary of this function goes here
 %   Detailed explanation goes here
 
-FILE_IN = 'audio_prueba.wav';
-%FILE_IN = 'audio_prueba_complejo.wav';
+%FILE_IN = 'audio_prueba.wav';
+FILE_IN = 'audio_prueba_complejo.wav';
 FILE_OUT = 'prueba_rapida_audio.wav';
+
+[y, Fs] = audioread(FILE_IN);
+L = y(:,1);
+R = y(:,2);
 
 Ts = 1/Fs;
 len = length(L);
@@ -12,11 +16,15 @@ max_time = (len*Ts)-Ts;
 t = 0:Ts:max_time;
 f = Fs*(0:(len/2))/len;
 
-[y, Fs] = audioread(FILE_IN);
-L = y(:,1);
-R = y(:,2);
+f_L = filter(Hd,L);
+f_R = filter(Hd,R);
 
+out = zeros(size(y));
 
+for i = 1:len
+    out(i,1) = f_L(i)^2;
+    out(i,2) = f_R(i)^2;
+end
 
 end
 
