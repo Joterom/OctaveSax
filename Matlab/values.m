f92dc0299dfa1589abcd4bfc6a84328854f09e81
Joterom <oterosaxo@gmail.com>
2018-10-16 13:02:49 +0200

    2.2.7

--- Matlab/values.m
+++ Matlab/values.m
@@ -1,39 +1,40 @@
-function [re_def, im_def] = values
+function [re_def, im_def, total] = values
 
-n1re = 0.72442626953125;
-n1im = -0.84814453125;
+n1re = 1;
+n1im = 0;
 
-n2re = 0.85797119140625;
-n2im = -0.43707275390625;
+n2re = -0.4539794921875;
+n2im = -0.8909912109375;
 
-n3re = 0.97503662109375;
-n3im = -0.0767822265625;
+n3re = -0.5877685546875;
+n3im = 0.80902099609375;
 
-n4re = 1.11798095703125;
-n4im = 0.36322021484375;
+n4re = 0.9876708984375;
+n4im = 0.15643310546875;
 
-n5re = 1.38409423828125;
-n5im = 1.18212890625;
+n5re = -0.30902099609375;
+n5im = -0.9510498046875;
 
-n6re = 2.76007080078125;
-n6im = 5.41680908203125;
+n6re = -0.70709228515625;
+n6im = 0.70709228515625;
 
-n7re = -0.31964111328125;
-n7im = -4.0614013671875;
+n7re = -0.9510498046875;
+n7im = 0.30902099609375;
 
-n8re = 0.50006103515625;
-n8im = -1.53875732421875;
+n8re = -0.15643310546875;
+n8im = -0.9876708984375;
 
-re = [n1re n2re n3re n4re n5re n6re n7re n8re];
-im = [n1im n2im n3im n4im n5im n6im n7im n8im];
+re_def = [n1re n2re n3re n4re n5re n6re n7re n8re];
+im_def = [n1im n2im n3im n4im n5im n6im n7im n8im];
 
-re_nor = re./abs(max(re));
-im_nor = im./abs(max(im));
-
-for i = 1:8
-    re_def(i) = sfi(re_nor(i),16,15);  
-    im_def(i) = sfi(im_nor(i),16,15);
-end
+total = [n1re+j*n1im n2re+i*n2im n3re+i*n3im n4re+i*n4im n5re+i*n5im n6re+i*n6im n7re+i*n7im n8re+i*n8im];
+% re_nor = re./abs(max(re));
+% im_nor = im./abs(max(im));
+% 
+% for i = 1:8
+%     re_def(i) = sfi(re_nor(i),16,15);  
+%     im_def(i) = sfi(im_nor(i),16,15);
+% end
 
 end
 
