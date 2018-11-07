52286774cc7aed8ff231d5ab541d073fee4267e0
Joterom <oterosaxo@gmail.com>
2018-11-07 14:18:04 +0100

    3.0.3

---
+++ Codigos referencia/testEllis.m
@@ -0,0 +1,49 @@
+function testEllis(FILE_IN)
+%MAIN Summary of this function goes here
+%   Detailed explanation goes here
+close all;
+
+f1 = figure('Name','Input and Output','NumberTitle','off');
+f2 = figure('Name','Frequency domain','NumberTitle','off');
+
+FILE_OUT = 'Octaved audio.wav';
+[in, Fs] = audioread(FILE_IN);
+
+in_mono = in(:,1);
+
+vocoder = pvoc(in_mono,2);
+out_prev = resample(vocoder,2,1);
+
+t = 1:1:length(in_mono);
+out = out_prev(1:length(in_mono));
+
+in_fft = fft(in_mono);
+out_fft = fft(out);
+
+in_fft_plot = 2*abs(in_fft(1:length(in_fft)/2+1));
+in_fft_plot(1) = in_fft(1);
+
+out_fft_plot = 2*abs(out_fft(1:length(out_fft)/2+1));
+out_fft_plot(1) = out_fft(1);
+
+f = 1:1:length(out_fft)/2+1;
+
+figure(f1)
+subplot(2,1,1), plot(t,in_mono), title('Input'), xlabel('Sample'), ylabel('Amplitude');
+xlim([0 length(t)]);
+ylim([-0.7 0.7]);
+subplot(2,1,2), plot(t,out), title('Output'), xlabel('Sample'), ylabel('Amplitude');
+xlim([0 length(t)]);
+ylim([-0.7 0.7]);
+
+figure(f2)
+subplot(2,1,1), plot(f,in_fft_plot), title('Input'), xlabel('Freq.');
+xlim([0 length(f)]);
+ylim([0 20000]);
+subplot(2,1,2), plot(f,out_fft_plot), title('Output'), xlabel('Freq.');
+xlim([0 length(f)]);
+ylim([0 20000]);
+
+audiowrite(FILE_OUT,out,Fs);
+end
+
