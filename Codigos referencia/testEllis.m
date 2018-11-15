5538aeecedbf6e0122042bc556f8dbb2c993704c
Joterom <oterosaxo@gmail.com>
2018-11-15 13:55:37 +0100

    3.1.1.2

--- Codigos referencia/testEllis.m
+++ Codigos referencia/testEllis.m
@@ -5,14 +5,16 @@ close all;
 
 f1 = figure('Name','Input and Output','NumberTitle','off');
 f2 = figure('Name','Frequency domain','NumberTitle','off');
+f3 = figure('Name','Ellis Vs FPGA','NumberTitle','off');
 
 FILE_OUT = 'Octaved audio.wav';
 FILE_MIX = 'Mixed audio.wav';
+FILE_OUT_FPGA = 'Octaved FPGA audio.wav';
 [in, Fs] = audioread(FILE_IN);
 
 in_mono = in(:,1);
 
-vocoder = pvoc(in_mono,2);
+vocoder = pvoc(in_mono,2,256);
 out_prev = resample(vocoder,2,1);
 
 t = 1:1:length(in_mono);
@@ -26,6 +28,8 @@ in_fft_plot(1) = in_fft(1);
 
 out_fft_plot = 2*abs(out_fft(1:length(out_fft)/2+1));
 out_fft_plot(1) = out_fft(1);
+
+out_fpga = algoritmo_final(in_mono);
 
 f = 1:1:length(out_fft)/2+1;
 
@@ -42,6 +46,14 @@ subplot(2,1,1), plot(f,in_fft_plot), title('Input'), xlabel('Freq.');
 xlim([0 length(f)]);
 ylim([0 20000]);
 subplot(2,1,2), plot(f,out_fft_plot), title('Output'), xlabel('Freq.');
+xlim([0 length(f)]);
+ylim([0 20000]);
+
+figure(f3)
+subplot(2,1,1), plot(t,out), title('Input'), xlabel('Freq.');
+xlim([0 length(f)]);
+ylim([0 20000]);
+subplot(2,1,2), plot(t,out_fpga), title('Output'), xlabel('Freq.');
 xlim([0 length(f)]);
 ylim([0 20000]);
 
