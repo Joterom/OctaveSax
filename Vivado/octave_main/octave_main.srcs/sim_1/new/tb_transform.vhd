1a049281e88e7b9a2304d6edd187975c6ddc765c
Joterom <j.oterom@alumnos.upm.es>
2019-04-03 07:36:54 +0200

    5.6

---
+++ Vivado/octave_main/octave_main.srcs/sim_1/new/tb_transform.vhd
@@ -0,0 +1,96 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tb_transform is
+--  Port ( );
+end tb_transform;
+
+architecture Behavioral of tb_transform is
+    component FFT_transform port (
+        aclk : IN STD_LOGIC;
+        aclken : IN STD_LOGIC;
+        s_axis_config_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        s_axis_config_tvalid : IN STD_LOGIC;
+        s_axis_config_tready : OUT STD_LOGIC;
+        s_axis_data_tdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        s_axis_data_tvalid : IN STD_LOGIC;
+        s_axis_data_tready : OUT STD_LOGIC;
+        s_axis_data_tlast : IN STD_LOGIC;
+        m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        m_axis_data_tuser : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        m_axis_data_tvalid : OUT STD_LOGIC;
+        m_axis_data_tready : IN STD_LOGIC;
+        m_axis_data_tlast : OUT STD_LOGIC;
+        event_frame_started : OUT STD_LOGIC;
+        event_tlast_unexpected : OUT STD_LOGIC;
+        event_tlast_missing : OUT STD_LOGIC;
+        event_status_channel_halt : OUT STD_LOGIC;
+        event_data_in_channel_halt : OUT STD_LOGIC;
+        event_data_out_channel_halt : OUT STD_LOGIC
+    ); end component;
+       
+    signal clk, enable_fft : STD_LOGIC;
+    signal event_frame_started, event_tlast_unexpected, event_tlast_missing, event_status_channel_halt, event_data_in_channel_halt
+           , event_data_out_channel_halt, config_tvalid, config_tready, input_tvalid, input_tready, input_tlast
+           , output_tvalid, output_tready, output_tlast : STD_LOGIC := '0';
+    signal config_data, output_user, im_in, re_in : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    signal input_data, output_data : STD_LOGIC_VECTOR(31 DOWNTO 0);
+begin
+    
+    FFT : FFT_transform port map (
+        aclk => clk,
+        aclken => enable_fft,
+        s_axis_config_tdata => config_data,
+        s_axis_config_tvalid => config_tvalid,
+        s_axis_config_tready => config_tready,
+        s_axis_data_tdata => input_data,
+        s_axis_data_tvalid => input_tvalid,
+        s_axis_data_tready => input_tready,
+        s_axis_data_tlast => input_tlast,
+        m_axis_data_tdata => output_data,
+        m_axis_data_tuser => output_user,
+        m_axis_data_tvalid => output_tvalid,
+        m_axis_data_tready => output_tready,
+        m_axis_data_tlast => output_tlast,
+        event_frame_started => event_frame_started,
+        event_tlast_unexpected => event_tlast_unexpected,
+        event_tlast_missing => event_tlast_missing,
+        event_status_channel_halt => event_status_channel_halt,
+        event_data_in_channel_halt => event_data_in_channel_halt,
+        event_data_out_channel_halt => event_data_out_channel_halt
+    );
+    
+    clk_gen : process
+        begin
+            clk <= '1';
+        wait for 5 ns;
+            clk <= '0';
+        wait for 5 ns;
+    end process;
+    
+    process
+        begin
+            enable_fft <= '1';
+        wait for 25 ns;   
+            config_data <= "0000001101010101";
+            config_tvalid <= '1';
+        wait for 100 ns;
+            --config_tvalid <= '0';
+            re_in <= "0100000000000000";
+            im_in <= "0000000000000000";
+            input_tvalid <= '1';
+        wait for 10 ns;
+            re_in <= "0000000000000000";
+            im_in <= "0000000000000000";
+        wait for 55 ns;
+            config_tvalid <= '0';
+        wait for 5040 ns;
+            input_tlast <= '1';
+        wait for 10 ns;
+            input_tlast <= '0';
+            output_tready <= '1';
+        wait;
+    end process;
+
+    input_data <= im_in & re_in;
+end Behavioral;
---
+++ Vivado/octave_main/octave_main.srcs/sources_1/new/transform_controller.vhd
@@ -0,0 +1,96 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity transform_controller is
+  Port ( 
+    clk : in STD_LOGIC;
+    reset : in STD_LOGIC;
+    enable_fft : in STD_LOGIC;
+    enable_ifft : in STD_LOGIC;
+    out_ctrl_for : out STD_LOGIC_VECTOR (21 downto 0)
+  );
+end transform_controller;
+
+architecture Behavioral of transform_controller is
+
+    component FFT_transform port (
+        aclk : IN STD_LOGIC;
+        aclken : IN STD_LOGIC;
+        s_axis_config_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        s_axis_config_tvalid : IN STD_LOGIC;
+        s_axis_config_tready : OUT STD_LOGIC;
+        s_axis_data_tdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        s_axis_data_tvalid : IN STD_LOGIC;
+        s_axis_data_tready : OUT STD_LOGIC;
+        s_axis_data_tlast : IN STD_LOGIC;
+        m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        m_axis_data_tuser : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        m_axis_data_tvalid : OUT STD_LOGIC;
+        m_axis_data_tready : IN STD_LOGIC;
+        m_axis_data_tlast : OUT STD_LOGIC;
+        event_frame_started : OUT STD_LOGIC;
+        event_tlast_unexpected : OUT STD_LOGIC;
+        event_tlast_missing : OUT STD_LOGIC;
+        event_status_channel_halt : OUT STD_LOGIC;
+        event_data_in_channel_halt : OUT STD_LOGIC;
+        event_data_out_channel_halt : OUT STD_LOGIC
+    ); end component;
+    
+    type transform_fsm is (IDLE, CONFIG, INPUT, OUTPUT, PROC);
+    signal fft_state, fft_state_next : transform_fsm;
+    signal config_tvalid, config_tready : STD_LOGIC := '0';
+    
+begin
+
+    FFT : FFT_transform port map (
+        aclk => clk,
+        aclken => enable_fft,
+        s_axis_config_tdata => config_tdata,
+        s_axis_config_tvalid => config_tvalid,
+        s_axis_config_tready => config_tready,
+        s_axis_data_tdata => input_data,
+        s_axis_data_tvalid => input_tvalid,
+        s_axis_data_tready => input_tready,
+        s_axis_data_tlast => input_tlast,
+        m_axis_data_tdata => output_tdata,
+        m_axis_data_tuser => output_tuser,
+        m_axis_data_tvalid => output_tvalid,
+        m_axis_data_tready => output_tready,
+        m_axis_data_tlast => output_tlast,
+        m_axis_status_tready => '1',
+        event_frame_started => event_frame_started,
+        event_tlast_unexpected => event_tlast_unexpected,
+        event_tlast_missing => event_tlast_missing,
+        event_status_channel_halt => event_status_channel_halt,
+        event_data_in_channel_halt => event_data_in_channel_halt,
+        event_data_out_channel_halt => event_data_out_channel_halt
+    );
+    
+--    reg : process(clk, reset)
+--        begin
+--            if reset = '1' then
+--                fft_state <= IDLE;
+--            elsif rising_edge(clk) then
+--                fft_state <= fft_state_next;
+--            end if;
+--    end process;
+    
+--    state_log : process(fft_state)
+--        begin
+--            case fft_state is
+--            when CONFIG =>
+--                if config_tready = '1' then
+--                    fft_state_next <= IDLE;
+--                end if;
+                
+--            when INPUT =>
+--            when OUTPUT =>
+--            when PROC =>
+--            when others => -- IDLE
+--                if config_tvalid = '1' and config_tready = '1' then
+--                    fft_state_next <= CONFIG;
+--                end if;
+--            end case;
+--    end process;
+ 
+end Behavioral;
